a0548e2cd8601ae5cc5560ab2ebb2b1b43f56f4b
smevawala <shivmevawala@gmail.com>
2014-03-26 15:25:26 -0400

    new scheme with 1 rx done

--- diversity.m
+++ diversity.m
@@ -11,7 +11,8 @@ BER_reg=zeros(length(SNR));
 BER_mrrc2=zeros(length(SNR));
 BER_mrrc4=zeros(length(SNR));
 
-n=3072; %number of samples
+
+n=307200; %number of samples
 m=2; %BPSK is 2-QAM
 
 % delayVector = [0 1 2 3 4] * 1e-5; % Discrete delays of four-path channel (s)
@@ -69,6 +70,13 @@ rchan_flat9.StoreHistory = 1; %If this value is 1, channel state information
 rchan_flat9.StorePathGains = 1;%If set to 1, the complex path gain vector is
 %stored.
 
+rchan_flat10=rayleighchan(1e-5,1e4); %Set up the channel fading object
+rchan_flat10.StoreHistory = 1; %If this value is 1, channel state information
+%needed by the channel visualization tool is stored. 
+rchan_flat10.StorePathGains = 1;%If set to 1, the complex path gain vector is
+%stored.
+
+
 %use the SNR to calculate EbNo for the normal sytem and the convolutional
 %coder
 EbNo = SNR -10*log10(log2(m));
@@ -83,62 +91,80 @@ for k=1:length(SNR)
     Y0=zeros(1,length(Y)/2);
     Y1=zeros(1,length(Y)/2);
     
-%     for kk=1:length(Y)
-%         if mod(kk,2) == 0 % number is even
-%             Y1(kk-1)=Y(kk);
-%             Y0(kk)=-conj(Y(kk));
-%         else
-%             %number is odd
-%             Y0(kk)=Y(kk);
-%             Y1(kk+1)=conj(Y(kk));
-%         end
-%         
-%     end
-
     for kk=1:length(Y)
         if mod(kk,2) == 0 % number is even
-            Y1(kk/2)=Y(kk);
+            Y1(kk-1)=Y(kk);
+            Y0(kk)=-conj(Y(kk));
         else
             %number is odd
-            Y0(kk/2+.5)=Y(kk);
+            Y0(kk)=Y(kk);
+            Y1(kk+1)=conj(Y(kk));
         end
         
     end
+% 
+%     for kk=1:length(Y)
+%         if mod(kk,2) == 0 % number is even
+%             Y1(kk/2)=Y(kk);
+%         else
+%             %number is odd
+%             Y0(kk/2+.5)=Y(kk);
+%         end
+%         
+%     end
     %add noise and rayleigh fading
     A=filter(rchan_flat,Y);
     A2=filter(rchan_flat2,Y);
     A3=filter(rchan_flat3,Y);
     A4=filter(rchan_flat4,Y);    
     
-    Anew0=filter(rchan_flat5,Y0);
-    Anew1=filter(rchan_flat6,Y1);   
-    
+    filter(rchan_flat5,Y0); %instantiate the Path Gains
+    filter(rchan_flat6,Y1);   
+%     Anewc0=filter(rchan_flat6,conj(Y0));
+%     Anewc1=filter(rchan_flat5,-conj(Y1));
     
     Ag = awgn(A, SNR(k),'measured');
     Ag2 = awgn(A2, SNR(k),'measured'); 
     Ag3 = awgn(A3, SNR(k),'measured'); 
     Ag4 = awgn(A4, SNR(k),'measured'); 
     
-    R0=awgn(Anew0+Anew1,SNR(k),'measured');
-    R0=awgn(Anew0+Anew1,SNR(k),'measured');
+    h0 = rchan_flat5.PathGains.'; 
+    h1 = rchan_flat6.PathGains.';
+    for kkk=1:(length(Y)/2)
+        h0(2*kkk) = h0(2*kkk-1);
+        h1(2*kkk) = h1(2*kkk-1);
+    end
+
+    Anew0=h0.*Y0;
+    Anew1=h1.*Y1; 
     
-    
+    Rnew=awgn(Anew0+Anew1,SNR(k),'measured');
+    DNew=zeros(1,length(Y));
+    for kkk=1:(length(Y)/2)
+        r0=Rnew(2*kkk-1);
+        r1=Rnew(2*kkk);
+
+        Dnew(2*kkk-1)= conj(h0(2*kkk-1))*r0 + h1(2*kkk)*conj(r1);
+        Dnew(2*kkk)= conj(h1(2*kkk-1))*r0-h0(2*kkk)*conj(r1);
+    end
     
     Ae=Ag.*conj(rchan_flat.PathGains.');
     Ae2=Ag2.*conj(rchan_flat2.PathGains.');
     Ae3=Ag3.*conj(rchan_flat3.PathGains.');
     Ae4=Ag4.*conj(rchan_flat4.PathGains.');
-    
+   
     %demodulate
     
     Z=qamdemod(Ae,m);
     Z_mrrc2=qamdemod(Ae+Ae2,m);
     Z_mrrc4=qamdemod(Ae+Ae2+Ae3+Ae4,m);
+    Z_new=qamdemod(Dnew,m);
     
     %calculate bit error rate
-    BER_reg(k)=biterr(Z,X)/(2*n);
-    BER_mrrc2(k)=biterr(Z_mrrc2,X)/(2*n);
-    BER_mrrc4(k)=biterr(Z_mrrc4,X)/(2*n);
+    BER_reg(k)=biterr(Z,X)/(n);
+    BER_mrrc2(k)=biterr(Z_mrrc2,X)/(n);
+    BER_mrrc4(k)=biterr(Z_mrrc4,X)/(n);
+    BER_new(k)=biterr(Z_new,X)/(n);
 end
 
 %plots
@@ -147,9 +173,9 @@ semilogy(EbNo, BER_reg,'kx');
 hold on;
 semilogy(EbNo, BER_mrrc2,'go');
 semilogy(EbNo, BER_mrrc4,'rx');
-
+semilogy(EbNo, BER_new,'bo');
 xlabel('EbNo (dB)')
 ylabel('BER')
 title('Waterfall Plots- Flat Fading')
-legend('no diversity', 'MRRC 2 RX', 'MRRC 4 RX')
+legend('no diversity', 'MRRC 2 RX', 'MRRC 4 RX','new 2tr')
 
