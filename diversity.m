fb96bb147a6a7f3e071f916f02ea7d08ba3b1d42
smevawala <shivmevawala@gmail.com>
2014-03-25 20:50:28 -0400

    added non working new scheme

--- diversity.m
+++ diversity.m
@@ -39,6 +39,36 @@ rchan_flat4.StoreHistory = 1; %If this value is 1, channel state information
 rchan_flat4.StorePathGains = 1;%If set to 1, the complex path gain vector is
 %stored.
 
+rchan_flat5=rayleighchan(1e-5,1e4); %Set up the channel fading object
+rchan_flat5.StoreHistory = 1; %If this value is 1, channel state information
+%needed by the channel visualization tool is stored. 
+rchan_flat5.StorePathGains = 1;%If set to 1, the complex path gain vector is
+%stored.
+
+rchan_flat6=rayleighchan(1e-5,1e4); %Set up the channel fading object
+rchan_flat6.StoreHistory = 1; %If this value is 1, channel state information
+%needed by the channel visualization tool is stored. 
+rchan_flat6.StorePathGains = 1;%If set to 1, the complex path gain vector is
+%stored.
+
+rchan_flat7=rayleighchan(1e-5,1e4); %Set up the channel fading object
+rchan_flat7.StoreHistory = 1; %If this value is 1, channel state information
+%needed by the channel visualization tool is stored. 
+rchan_flat7.StorePathGains = 1;%If set to 1, the complex path gain vector is
+%stored.
+
+rchan_flat8=rayleighchan(1e-5,1e4); %Set up the channel fading object
+rchan_flat8.StoreHistory = 1; %If this value is 1, channel state information
+%needed by the channel visualization tool is stored. 
+rchan_flat8.StorePathGains = 1;%If set to 1, the complex path gain vector is
+%stored.
+
+rchan_flat9=rayleighchan(1e-5,1e4); %Set up the channel fading object
+rchan_flat9.StoreHistory = 1; %If this value is 1, channel state information
+%needed by the channel visualization tool is stored. 
+rchan_flat9.StorePathGains = 1;%If set to 1, the complex path gain vector is
+%stored.
+
 %use the SNR to calculate EbNo for the normal sytem and the convolutional
 %coder
 EbNo = SNR -10*log10(log2(m));
@@ -50,23 +80,61 @@ for k=1:length(SNR)
     X=randi([0 m-1],1,n);
     %modulate
     Y=qammod(X,m);
+    Y0=zeros(1,length(Y)/2);
+    Y1=zeros(1,length(Y)/2);
+    
+%     for kk=1:length(Y)
+%         if mod(kk,2) == 0 % number is even
+%             Y1(kk-1)=Y(kk);
+%             Y0(kk)=-conj(Y(kk));
+%         else
+%             %number is odd
+%             Y0(kk)=Y(kk);
+%             Y1(kk+1)=conj(Y(kk));
+%         end
+%         
+%     end
+
+    for kk=1:length(Y)
+        if mod(kk,2) == 0 % number is even
+            Y1(kk/2)=Y(kk);
+        else
+            %number is odd
+            Y0(kk/2+.5)=Y(kk);
+        end
+        
+    end
     %add noise and rayleigh fading
     A=filter(rchan_flat,Y);
     A2=filter(rchan_flat2,Y);
     A3=filter(rchan_flat3,Y);
     A4=filter(rchan_flat4,Y);    
+    
+    Anew0=filter(rchan_flat5,Y0);
+    Anew1=filter(rchan_flat6,Y1);   
+    
+    
     Ag = awgn(A, SNR(k),'measured');
     Ag2 = awgn(A2, SNR(k),'measured'); 
     Ag3 = awgn(A3, SNR(k),'measured'); 
     Ag4 = awgn(A4, SNR(k),'measured'); 
+    
+    R0=awgn(Anew0+Anew1,SNR(k),'measured');
+    R0=awgn(Anew0+Anew1,SNR(k),'measured');
+    
+    
+    
     Ae=Ag.*conj(rchan_flat.PathGains.');
     Ae2=Ag2.*conj(rchan_flat2.PathGains.');
     Ae3=Ag3.*conj(rchan_flat3.PathGains.');
     Ae4=Ag4.*conj(rchan_flat4.PathGains.');
+    
     %demodulate
+    
     Z=qamdemod(Ae,m);
     Z_mrrc2=qamdemod(Ae+Ae2,m);
     Z_mrrc4=qamdemod(Ae+Ae2+Ae3+Ae4,m);
+    
     %calculate bit error rate
     BER_reg(k)=biterr(Z,X)/(2*n);
     BER_mrrc2(k)=biterr(Z_mrrc2,X)/(2*n);
